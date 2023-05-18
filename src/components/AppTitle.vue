e88af7ec8fbba79633ed2e9b54a0d5f7693949e0
Bruno Canini <brunocanini1994@gmail.com>
2023-05-18 17:58:30 +0200

    first commit

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: "AppTitle",
+}
+</script>
+
+<template>
+    <h1 class="green">La mia prima app con <span class="yellow">Vite!</span></h1>
+</template>
+
+<style scoped>
+h1>span.yellow {
+    color: yellow;
+    text-decoration: underline;
+}
+</style>
